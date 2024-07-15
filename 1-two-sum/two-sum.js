9715acd7760c311e43c4c713f7b5df162b9b6e85
Fahad Masood <fahadmasood03@gmail.com>
2024-07-16 01:12:10 +0530

    Time: 133 ms (15.33%) | Memory: 49.4 MB (77.83%) - LeetSync

---
+++ 1-two-sum/two-sum.js
@@ -0,0 +1,17 @@
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[]}
+ */
+var twoSum = function(nums, target) {
+    let left = 0, right = nums.length - 1;
+
+    for(let i = 0; i < nums.length - 1; i++){
+        for(j = i+1; j < nums.length; j++){
+            if(nums[i] + nums[j] === target){
+                return [i, j]
+            }
+        }
+    }
+    return []
+};
